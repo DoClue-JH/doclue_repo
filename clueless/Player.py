c850f71a331cee2b7dd6f8590a64e5d0b7e0898c
megan <meganmli18@gmail.com>
2023-03-07 20:15:14 -0800

    Added getter and setter for Player status

--- clueless/Player.py
+++ clueless/Player.py
@@ -6,11 +6,16 @@ class Player:
     def __init__(self, name):
         self.player_name = name
         self.player_status = 0 # 0 = active, 1 = lost, and 2 = inactive
+        self.first_turn = True # Set to false after first turn
         # self.cards = 
         
-        # self.player_location = # use a dict to map name to location
-        
-
+        # Use a dictionary to map name to location, how are we defining locations?
+        # self.player_location = 
+       
+    # Return player status (0 = active, 1 = lost, and 2 = inactive)
+    def get_status(self):
+        return self.player_status
+    
     # Update player status (0 = active, 1 = lost, and 2 = inactive)
     def set_status(self, status):
         self.player_status = status
