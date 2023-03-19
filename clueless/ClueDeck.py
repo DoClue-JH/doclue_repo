5f8af94a89e30b69596e8519f4c4dc5486c08793
Kweku Awuah <awuahsparkels@gmail.com>
2023-03-19 15:48:14 -0400

    Added weapon class and Deck class

---
+++ clueless/ClueDeck.py
@@ -0,0 +1,66 @@
+import random
+from weapon import Weapon
+
+class ClueDeck:
+    def __init__(self, players):
+        self.players = players
+        self.characters = ["Miss Scarlett", "Colonel Mustard", "Mrs. White", "Mr. Green", "Mrs. Peacock", "Professor Plum"]
+        self.rooms = ["Kitchen", "Ballroom", "Conservatory", "Dining Room", "Billiard Room", "Library", "Lounge", "Hall", "Study"]
+        self.weapons = [weapon1, weapon2, weapon3, weapon4, weapon5, weapon6]
+
+        # Choose one random character, room, and weapon for the secret deck
+        self.secret_deck = [
+            self.characters.pop(random.randint(0, len(self.characters)-1)),
+            self.rooms.pop(random.randint(0, len(self.rooms)-1)),
+            self.weapons.pop(random.randint(0, len(self.weapons)-1))
+        ]
+
+        #Add all cards and shuffle
+        self.deck = self.characters + self.rooms + self.weapons
+        random.shuffle(self.deck)
+
+    # Deal out cards to players 
+    def deal(self):
+        num_players = len(self.players)
+        
+        # Create dictionary to hold dealt cards for each player
+        dealt_cards = {}
+        for i in range(num_players):
+            dealt_cards[self.players[i]] = []
+
+        # Deal cards to players
+        for i in range(num_players):
+            for player in dealt_cards:
+                if len(self.deck) == 0:
+                    break
+                card = self.deck.pop(0)
+                dealt_cards[player].append(card)
+
+        # Deal remaining cards in round-robin fashion
+        i = 0
+        while len(self.deck) > 0:
+            player = list(dealt_cards.keys())[i]
+            card = self.deck.pop(0)
+            dealt_cards[player].append(card)
+            i = (i+1) % num_players
+        
+        # Return a dictionary in the format of player: [dealt cards]
+        return dealt_cards
+    
+# Instantiate    
+weapon1 = Weapon("Rope")
+weapon2 = Weapon("Lead Pipe")
+weapon3 = Weapon("Knife")
+weapon4 = Weapon("Wrench")
+weapon5 = Weapon("Candlestick")
+weapon6 = Weapon("Revolver")
+
+players = ["Katie", "Khue", "Kweku", "Megan", "Sely"]
+deck = ClueDeck(players)
+dealt_cards = deck.deal()
+
+for key, value in dealt_cards.items():
+    print(f"{key}: {value}")
+print()
+print("Secret deck:", deck.secret_deck)
+
---
+++ clueless/weapon.py
@@ -0,0 +1,6 @@
+class Weapon:
+    def __init__(self, name):
+        self.name = name
+    
+    def __repr__(self):
+        return self.name
