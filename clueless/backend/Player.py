20e2cfa8e3f4b3d592c9d90bfb247009af42d3dd
megan <meganmli18@gmail.com>
2023-04-13 15:25:27 -0700

    Cleaned up Player class

--- clueless/backend/Player.py
+++ clueless/backend/Player.py
@@ -1,34 +1,19 @@
 class Player:
     
     def __init__(self, player_name, player_id, player_current_location, player_old_location, player_hand, turn_status, player_status, player_notebook):
-        # player_name : str
-        # player_id : int
-        # player_current_location : Tile
-        # player_old_location : Tile
-        # player_hand : Deck
-        # turn_status : TURN_STATUS
-        # player_status : PLAYER_STATUS
-        # player_notebook : dict
-        
-        self.player_name = player_name
-        self.player_id = player_id
-        self.player_current_location = player_current_location
-        self.player_old_location = player_old_location
-        self.player_hand = player_hand
-        self.turn_status = turn_status
-        self.player_status = player_status
-        self.player_notebook = player_notebook
+        self.player_name = player_name                          # string
+        self.player_id = player_id                              # int
+        self.player_current_location = player_current_location  # Tile
+        self.player_old_location = player_old_location          # Tile
+        self.player_hand = player_hand                          # Deck
+        self.turn_status = turn_status                          # Enum (int)
+        self.player_status = player_status                      # Enum (int)
+        self.player_notebook = player_notebook                  # Dict
 
-    def update():
-        pass
-
+    ''' GETTER FUNCTIONS '''
     # Returns the player's status corresponding to actively playing, passively playing (lost), or unchosen
     def get_player_status(self):
         return self.player_status
-
-    # Update the player's status
-    def set_player_status(self, PLAYER_STATUS):
-        self.player_status = PLAYER_STATUS
     
     # Returns the player's notebook
     def get_notebook(self):
@@ -37,3 +22,18 @@ class Player:
     # Returns the player's hand, Deck of Cards
     def get_hand(self):
         return self.player_hand
+    
+    
+    ''' SETTER FUNCTIONS '''
+    # Update the player's old location and current location
+    def update(self, new_location):
+        self.player_old_location = self.player_current_location
+        self.player_current_location = new_location
+    
+    # Update the player's status
+    def set_player_status(self, PLAYER_STATUS):
+        self.player_status = PLAYER_STATUS
+    
+    # Update the player's turn status
+    def set_player_status(self, TURN_STATUS):
+        self.turn_status = TURN_STATUS
