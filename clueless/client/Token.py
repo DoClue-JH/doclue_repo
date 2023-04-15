b02b529f1cb2b7f08c6c621d28dfb6e19d55e21b
megan <meganmli18@gmail.com>
2023-04-15 09:36:51 -0700

    Removed backend player attr from Token class

--- clueless/client/Token.py
+++ clueless/client/Token.py
@@ -5,19 +5,14 @@ class Token(pygame.sprite.Sprite):
     
     def __init__(self, name, pos_x, pos_y, width, height, color):
         super().__init__()
-        
-        self.player_name = name
-        # Initialize player to active (0 = active, 1 = lost, and 2 = inactive)
-        self.player_status = 0 
-        self.first_turn = True # Set to false after first turn
-        self.color = color
 
+        self.player_name = name
+        self.color = color
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.width = width
         self.height = height
         self.vel = 3
-        # self.player_location = 
     
     # def draw(self, window):
     #     pygame.draw.rect(window, self.color, self.rect)
